e5752ad084ed01684f2241858e0053e641b018af
Katsumi-N <naya-katsumi@i.softbank.jp>
2021-01-21 18:20:11 +0900

    add command line arguments and refactoring

--- simulate.py
+++ simulate.py
@@ -1,12 +1,13 @@
-import PySimpleGUI as sg
-from pokeca_scraping import scraping
-import random
-
 # 参考にしたサイト
 # Qiita "PySimpleGUIの基本的な使用方法"https://qiita.com/KatsunoriNakamura/items/376da645e52f7ef7f9ef
 # Trinket "Graph Element" https://pysimplegui.trinket.io/demo-programs#/graph-element/graph-element-drawing-and-dragging
+import argparse
+import PySimpleGUI as sg
+import random
+from pokeca_scraping import scraping
 
-def start(graph, deck_list, deck_img, deck_list_copy):
+def start(graph, deck_copy, deck_list_copy):
+    deck = deck_copy
     deck_list = deck_list_copy[:]
     random.shuffle(deck_list)
 
@@ -16,28 +17,42 @@ def start(graph, deck_list, deck_img, deck_list_copy):
 
     # 手札を7枚引く
     for i in range(7):
-        graph.draw_image(filename=deck_img[hand[i]], location=(150+ i*70, 100))
+        graph.draw_image(filename=deck[hand[i]][2], location=(150+ i*70, 100))
+        deck[hand[i]][0] -= 1
     # サイド
     side_loc = [(110, 250), (180, 250), (110, 330), (180, 330), (110, 410), (180, 410)]
     for i in range(6):
-        graph.draw_image(filename=deck_img[side[i]], location=side_loc[i])
+        graph.draw_image(filename=deck[side[i]][2], location=side_loc[i])
+        deck[side[i]][0] -= 1
 
-    return deck_list
+    return deck, deck_list
 
 def shuffle(deck_list):
     random.shuffle(deck_list)
     print("shuffle")
 
-def draw(graph, deck_list, deck_img):
+def draw(graph, deck, deck_list):
     draw_card = deck_list.pop()
-    graph.draw_image(filename=deck_img[draw_card], location=(680, 230))
-
-# def see_deck(deck_list, deck_img):
-
+    deck[draw_card][0] -= 1
+    graph.draw_image(filename=deck[draw_card][2], location=(680, 230))
 
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--deck_code",
+        type=str,
+        default="kVVF5w-bueC8Z-FFVvkF",
+    )
+    parser.add_argument(
+        "--driver_dir",
+        type=str,
+        default="C:\\Users\\naya-\\Anaconda3\\envs\\rl\\Lib\\site-packages\\chromedriver_binary\\chromedriver.exe"
+    )
+    args = parser.parse_args()
+
+
     sg.theme('Dark Blue 3')
     layout = [[sg.Graph(
         canvas_size=(900, 500),
@@ -47,8 +62,8 @@ def main():
         change_submits=True,  # mouse click events
         background_color='lightblue',
         drag_submits=True)],
-        [sg.Button("Reset"), sg.Button("Shuffle"), sg.Button("Draw"), sg.Button("See Deck"), 
-         sg.Button("Pick 1 card"),sg.Button("Exit")]
+        [sg.Button("Reset"), sg.Button("Shuffle"), sg.Button("Draw"), sg.Button("See Deck"),
+         sg.Button("Pick 1 card"), sg.Button("Exit")]
     ]
 
     window = sg.Window("Pokemon-card simulator", layout, finalize=True)
@@ -77,10 +92,14 @@ def main():
     dragging = False
     start_point = end_point = prior_rect = None
 
-    _, deck_list, deck_img = scraping()
-    deck_list_copy = deck_list[:]
+    #deck(キーが名前、値が枚数), deck_list(カードの名前を格納したリスト)を取得
+    deck, deck_list = scraping(args.deck_code, args.driver_dir)
 
-    deck_list = start(graph, deck_list, deck_img, deck_list_copy)
+    #デッキ(キーが名前、値が枚数)とデッキリスト(リスト)のコピーを作成
+    deck_list_copy = deck_list[:]
+    deck_copy = deck.copy()
+
+    deck, deck_list = start(graph, deck_copy, deck_list_copy)
 
     while True:
         event, values = window.read()
@@ -110,30 +129,27 @@ def main():
 
         if event == "Reset":
             graph.erase()
-            deck_list = start(graph, deck_list, deck_img, deck_list_copy)
-
+            deck, deck_list = start(graph, deck_copy, deck_list_copy)
 
         if event == "Shuffle":
             shuffle(deck_list)
-            print("デッキ枚数",len(deck_list))
 
         if event == "Draw":
-            draw(graph, deck_list, deck_img)
-            print("デッキ枚数",len(deck_list))
+            draw(graph, deck, deck_list)
 
         if event == "See Deck":
-            sorted_deck = sorted(deck_list)
-            for i in sorted_deck:
-                print(i)
+            print(deck)
 
         if event == "Pick 1 card":
             card_name = sg.PopupGetText('デッキから持ってくるカード名を入力してください')
             # assert card_name in set(deck_list), 'デッキ内のカードを指定してください'
             if card_name in set(deck_list):
                 deck_list.remove(card_name)
-                graph.draw_image(filename=deck_img[card_name], location=(680, 230))
+                graph.draw_image(filename=deck[card_name][2], location=(680, 230))
+                deck[card_name][0] -= 1
             else:
                 print('デッキ内のカードを指定してください')
+
         if event == "Exit":
             break
 
